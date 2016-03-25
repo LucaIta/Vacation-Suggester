84bfad56380362377f420575d1c4ef3692c06135
Luca Quatela <lucaqq@gmail.com>
2016-03-25 08:46:39 -0700

    add the compiled form, now it can collect data from the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault;
-    alert($("#age").val());    
+    var userAgeInput = ($("#userAge").val());
+    alert (userAgeInput);
+    var userFavoritePetInput = ($("#userFavoritePet").val());
+    alert (userFavoritePetInput);
+    var userFavoriteCountryInput = ($("#userFavoriteCountry").val());
+    alert (userFavoriteCountryInput);
+    var userFavoriteIceCreamFlavorInput = ($("#userFavoriteIceCreamFlavor").val());
+    alert (userFavoriteIceCreamFlavorInput);
+    var userFavoriteSportInput = ($("#userFavoriteSport").val());
+    alert (userFavoriteSportInput);
   })
 })
